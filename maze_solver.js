65489c0f99b0f7c35ec3c54b7c6e3ac7a6f86ce1
vapo94b <vapo94b@gmail.com>
2023-11-23 15:00:52 +0100

    Prints if the maze if solvable

--- maze_solver.js
+++ maze_solver.js
@@ -2,6 +2,7 @@ const direction = [[1,0],[-1,0],[0,-1],[0,1]];
 const fieldCharacter = '░';
 const pathCharacter = '*';
 const hat = '^';
+const { fieldA }  = require('./main.js');
 
 
 class solve {
@@ -14,10 +15,11 @@ class solve {
             if(this.field[newPosition[0]][newPosition[1]]==fieldCharacter) {
                 this.field[newPosition[0]][newPosition[1]] = pathCharacter;
                 let newDirection = [0,0];
-                for(let i = 0; i < 4; i++) {
-                    newDirection = (newPosition,direction)=>{return [newPosition[0]+direction[0],newPosition[1]+direction[1]];};
-                    console.log(typeof(newDirection));
-                    if(position[0]!=newDirection[0] & position[1]!=newDirection[1]) {
+                for(let i = 0; i < direction.length; i++) {
+                    newDirection[0] = parseInt(newPosition[0])+parseInt(direction[i][0]);
+                    newDirection[1] = parseInt(newPosition[1])+parseInt(direction[i][1]);
+                    //console.log(newPosition,newDirection,position[0]!=newDirection[0],parseInt(position[1])!=newDirection[1]);
+                    if(parseInt(position[0])!=newDirection[0] | parseInt(position[1])!=newDirection[1]) {
                         if(this.check(newPosition,newDirection)==true) {
                             result=true;
                             break;
@@ -25,12 +27,14 @@ class solve {
                     }
                 }
             } else if(this.field[newPosition[0]][newPosition[1]]==hat) {
-                console.log("I found the hut!")
+                console.log("I found the hut!");
+                console.log(this.field);
                 return true;
             }
         }
     return result;  
     }
 }
-const solver =  new solve([['░','░','^'],['O','O','O'],['O','O','O']]);
-console.log(solver.check());
+fieldA[0][0] = fieldCharacter;
+const solver =  new solve(fieldA);
+console.log(solver.check(),fieldA);
