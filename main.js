c0325099ca8c7e3b92ddc9ab7854b5315e0b5bbd
vapo94b <vapo94b@gmail.com>
2023-11-25 22:40:01 +0100

    Generates a random and solvable field

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 const step = {"d":[1,0],"u":[-1,0],"l":[0,-1],"r":[0,1]};
+const direction = [[1,0],[-1,0],[0,-1],[0,1]];
+
 
 class Field {
   constructor(field) {
@@ -60,34 +62,58 @@ class Field {
     }
   }
   static generateField(height, width, percentage) {
-    let col = height;
-    let row = width;
-    let totalSpace = col*row;
-    let totalHut = Math.floor(percentage*totalSpace);
-    let twodimArray = new Array(col).fill(null).map(() => new Array(row).fill(fieldCharacter));
-    let randomNumbers = new Set();
-    while(randomNumbers.size < totalHut) {
+      let col = height;
+      let row = width;
+      let totalSpace = col*row;
+      let totalHut = Math.floor(percentage*totalSpace);
+      do {
+      var twodimArray = new Array(col).fill(null).map(() => new Array(row).fill(fieldCharacter)); 
+      let randomNumbers = new Set();
+      while(randomNumbers.size < totalHut) {
         randomNumbers.add(Math.floor(Math.random()*(totalSpace-2)+2));
-    }
-    const randomNumbersArray = Array.from(randomNumbers);
-    twodimArray[0][0]=pathCharacter;
-    let sign = hole;
-    randomNumbersArray.forEach(
+      }
+      let randomNumbersArray = Array.from(randomNumbers);
+      //randomNumbers.clear();
+      let sign = hole;
+      randomNumbersArray.forEach(
         function(randomNum,index) {
             if(index==randomNumbersArray.length-1) {
                 sign=hat;
             }
             if(randomNum%row==0) {twodimArray[Math.trunc(randomNum/row)-1][width-1]=sign;}
             else {twodimArray[Math.trunc(randomNum/row)][(randomNum%row-1)]=sign;};
-        });
+          });
+        } while(Field.check([0,0],[0,0],JSON.parse(JSON.stringify(twodimArray)))==false);
+      twodimArray[0][0]=pathCharacter;
     return twodimArray;
+}
+  static check(position=[0,0],newPosition=[0,0],field) {
+    let result = false;
+    if((newPosition[0] >= 0) & (newPosition[0] < field.length) & (newPosition[1]>=0) & (newPosition[1]< field[0].length) ) {
+      if(field[newPosition[0]][newPosition[1]]==fieldCharacter) {
+            field[newPosition[0]][newPosition[1]] = pathCharacter;
+            let newDirection = [0,0];
+            for(let i = 0; i < direction.length; i++) {
+                newDirection[0] = parseInt(newPosition[0])+parseInt(direction[i][0]);
+                newDirection[1] = parseInt(newPosition[1])+parseInt(direction[i][1]);
+                if(parseInt(position[0])!=newDirection[0] | parseInt(position[1])!=newDirection[1]) {
+                    if(Field.check(newPosition,newDirection,field)==true) {
+                        return true;
+                    }
+                }
+            }
+            if(result==false) {
+                field[newPosition[0]][newPosition[1]] = fieldCharacter;
+            }
 
+        } else if(field[newPosition[0]][newPosition[1]]==hat) {
+            return true;
+        }
+    }
+    return result;  
   }
 }
-const fieldA = Field.generateField(5,5,0.4);
-/*
-const myField = new Field();
+const fieldA = Field.generateField(10,10,0.4);
+const myField = new Field(fieldA);
 myField.startGame();
-*/
 //setInterval(()=>{process.stdout.write('\x1Bc')},3000);
-//myField.print(Field.generateField(3,4,0.3));
